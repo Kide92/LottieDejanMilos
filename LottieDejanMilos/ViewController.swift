99ab2839586b782e6e6dd3cc7a54b3917ef4ff06
Dejan Matovic <matovic_92@yahoo.com>
2019-02-07 10:05:59 +0100

    Initial Commit

---
+++ LottieDejanMilos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottieDejanMilos
+//
+//  Created by Dejan Matovic on 2/7/19.
+//  Copyright © 2019 Dejan Matovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
